de696a37d9990802fc6b22591e95cf5db113e265
Fredy Laksmono <fredy.laksmono@gmail.com>
2022-12-19 14:45:36 -0600

    Initial phase data inputted

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from "next/head";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { Inter } from "@next/font/google";
+import { AiFillGithub, AiFillYoutube, AiFillLinkedin } from "react-icons/ai";
+import Image from "next/image";
+import flPic from "../public/FL_PP_2022.jpg";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,7 @@ export default function Home() {
       <main className=" bg-white px-10 ">
         <section className=" min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl">developedbyfl</h1>
+            <h1 className="text-xl font-titleFont">DEVELOPEDBYFL</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill className=" cursor-pointer text-xl" />
@@ -31,6 +34,26 @@ export default function Home() {
               </li>
             </ul>
           </nav>
+          <div className=" text-center p-10">
+            <h2 className=" text-5xl py-2 text-teal-600 font-medium">
+              Fredy Laksmono
+            </h2>
+            <h3 className="text-2xl py-2">Software Developer</h3>
+            <p className="text-md py-5 leading-8 text-gray-800 ">
+              Software Developer with a decade of experience as consultant in
+              Identity Management and Enterprise Resource Planning. I have a
+              passion for learning new technologies and building applications
+              that solve real world problems.
+            </p>
+          </div>
+          <div className=" text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <AiFillLinkedin />
+            <AiFillGithub />
+            <AiFillYoutube />
+          </div>
+          <div className="relative mx-auto w-52 mt-20">
+            <Image src={flPic} />
+          </div>
         </section>
       </main>
     </>
