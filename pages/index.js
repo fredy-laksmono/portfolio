3bfb3bc4025f32bba2befb48961425feb333866f
Fredy Laksmono <fredy.laksmono@gmail.com>
2023-02-02 14:03:04 -0600

    Fix: Typo on footer

--- pages/index.js
+++ pages/index.js
@@ -267,9 +267,9 @@ export default function Home() {
           </div>
         </section>
       </main>
-      <footer className=" bg-white px-10 md:px-20 lg:px-40 pt-20 pb-5 dark:bg-slate-800 dark:text-white">
+      <footer className=" bg-white px-10 md:px-20 lg:px-40 pt-10 pb-5 dark:bg-slate-800 dark:text-white">
         <p className="text-center">Created by Next.js & Tailwind CSS</p>
-        <p className="text-center">© 2023 DEVELOPEDFBYFL</p>
+        <p className="text-center">© 2023 DEVELOPEDBYFL</p>
       </footer>
     </div>
   );
